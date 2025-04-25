bae8aa01c3d58726dc97f293355fb57e2cf7ad2f
Hamza Aboumoussa <aboumoussahamza2@gmail.com>
2025-04-25 14:24:41 +0200

    Fix : Add content field to question

--- Server/Domain/Entities/Question.cs
+++ Server/Domain/Entities/Question.cs
@@ -7,6 +7,7 @@ namespace Domain.Entities
         [JsonPropertyName("id")]
         public required string Id { get; set; }
         public required string QuizId { get; set; }
+        public required string Content { get; set; }
         public required Quiz Quiz { get; set; }
         public DateTimeOffset GeneratedAt { get; set; }
         public required string Type { get; set; } // "Checkbox", "Multiple Choice" or "Open"
