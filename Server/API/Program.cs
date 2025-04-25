b4230f267061b968eab29e317ca7b7e46a4e99ad
Hamza Aboumoussa <aboumoussahamza2@gmail.com>
2025-04-25 17:56:04 +0200

    Add a n extension to service collection to add service

--- Server/API/Program.cs
+++ Server/API/Program.cs
@@ -1,105 +1,21 @@
-using Microsoft.EntityFrameworkCore;
-using Infrastructure.Repository;
-using Domain.Interfaces;
-using Infrastructure.Service;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Application.CommandHandlers;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.HttpLogging;
+using API;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Auth", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        Description = "Enter your Bearer access token here"
-    });
-     
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        { new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Auth"
-                }
-            }, new List<string>() }
-    });
-});
+builder.Services.AddPresentationServices();
 
-builder.Services.AddMediatR(cfg =>
-    cfg.RegisterServicesFromAssembly(typeof(LoginHandler).Assembly));
+builder.Services.AddApplicationServices();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseCosmos(
-        builder.Configuration.GetConnectionString("CosmosDb")!,
-        databaseName: "TeachingPlatforme"));
-        
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<ICourseRepository, CourseRepository>();
-builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
-builder.Services.AddScoped<IPdfToMarkdownService, PdfToMarkdownService>();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+builder.Services.AddAuthenticationServices(builder.Configuration);
 
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["Jwt:Audience"],
+builder.Services.AddHttpLoggingServices();
 
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
-
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.FromMinutes(1)
-        };
-    });
-
-// Register the HTTP logging services
-builder.Services.AddHttpLogging(options =>
-{
-    options.LoggingFields = HttpLoggingFields.None; // Log all available fields
-    options.RequestBodyLogLimit = 4096; // Limit request body size to 4 KB
-    options.ResponseBodyLogLimit = 4096; // Limit response body size to 4 KB
-    options.CombineLogs = true; // Combine request and response logs into a single entry
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "AllowOrigin",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:3000")
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials();
-        });
-});
+builder.Services.AddCorsServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
